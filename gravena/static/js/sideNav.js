83e2f7075aaea339488213e902cf9a155bdef24d
Mohamed Salem <mo.zaher.salem@gmail.com>
2016-08-30 15:05:21 +0300

    Add side nave menu in sanitary ware page

---
+++ gravena/static/js/sideNav.js
@@ -0,0 +1,6 @@
+// $(function(){
+// 	$('.side-nav-hover').hover(function(){
+// 		$('#side-nav').css('transform', 'translate(0, 0)');
+// 		$(this).css('background-color', 'red');
+// 	})
+// })();
