b49f628c7b5a98ecf863a73718a827f69c35ffe6
Mohamed Salem <mo.zaher.salem@gmail.com>
2016-09-06 11:52:49 +0300

    Side nav Created sideNav.html - imported as <%include%> side-nav animation sideNav.js

--- gravena/static/js/project.js
+++ gravena/static/js/project.js
@@ -2,16 +2,4 @@
 
 
 // Side-nav Animation
-$(function(){
-	$('.side-nav-hover').hover(function(){
-		$('#side-nav').css('transform', 'translate(0, 0)');
-		// $(this).css('background-color', 'red');
-	})
 
-	var side_nav_container_width = $('.side-nav-container').innerWidth();
-	console.log(side_nav_container_width);
-
-	$('#side-nav').mouseleave(function(){
-		$(this).css('transform', 'translate(-85%, 0)');
-	})
-});
--- gravena/static/js/sideNav.js
+++ gravena/static/js/sideNav.js
@@ -1,6 +1,13 @@
-// $(function(){
-// 	$('.side-nav-hover').hover(function(){
-// 		$('#side-nav').css('transform', 'translate(0, 0)');
-// 		$(this).css('background-color', 'red');
-// 	})
-// })();
+$(function(){
+	$('.side-nav-hover').hover(function(){
+		$('#side-nav').css('transform', 'translate(0, 0)');
+		// $(this).css('background-color', 'red');
+	})
+
+	var side_nav_container_width = $('.side-nav-container').innerWidth();
+	console.log(side_nav_container_width);
+
+	$('#side-nav').mouseleave(function(){
+		$(this).css('transform', 'translate(-90%, 0)');
+	})
+});
