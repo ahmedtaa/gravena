cfc27d07ed9c5a35bec011782282e5f018a37cc9
Muhammad Yossry <mohamedyosry3000@gmail.com>
2016-10-30 22:49:12 +0200

    Seperate products into a seperate app

---
+++ products/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def sanitary_ware(request):
+    thumnail_image = []
+    main_img = None
+    model_name = ''
+    model_description = ''
+    technical drawings = []
+    context = {'dada': 14}
+    return render(request, 'pages/sanitary_ware.html', context)
+
+def acrylic_bathtubs(request):
+    return render(request, 'pages/acrylic_bathtubs.html')
+
+def water_taps(request):
+    return render(request, 'pages/water_taps.html')
+
+def additional_items(request):
+    return render(request, 'pages/additional_items.html')
