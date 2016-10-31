e8dab81c2a270136096b8a23a4eabfbed079ba36
Muhammad Yossry <mohamedyosry3000@gmail.com>
2016-10-31 21:44:38 +0200

    Add products to views

--- products/views.py
+++ products/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 
+from .models import Category, Product
+
 def home(request):
     return render(request, 'pages/home.html')
 
 def sanitary_ware(request):
-    thumnail_image = []
-    main_img = None
-    model_name = ''
-    model_description = ''
-    technical_drawings = []
-    context = {'dada': 14}
-    return render(request, 'pages/sanitary_ware.html', context)
+    category = Category.objects.get(name='Sanitary Ware')
+    products = category.products.all()
+    return render(request, 'pages/sanitary_ware.html', {'products': products})
 
 def acrylic_bathtubs(request):
-    return render(request, 'pages/acrylic_bathtubs.html')
+    category = Category.objects.get(name='Acrylic Bath Tubs')
+    products = category.products.all()
+    return render(request, 'pages/acrylic_bathtubs.html', {'products': products})
 
 def water_taps(request):
-    return render(request, 'pages/water_taps.html')
+    category = Category.objects.get(name='Water Taps')
+    products = category.products.all()
+    return render(request, 'pages/water_taps.html', {'products': products})
 
 def additional_items(request):
-    return render(request, 'pages/additional_items.html')
+    category = Category.objects.get(name='Additional Items')
+    products = category.products.all()
+    return render(request, 'pages/additional_items.html', {'products': products})
+
