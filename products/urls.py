53ed53282388f1f0de36c4242374ca5e98bd224f
Muhammad Yossry <mohamedyosry3000@gmail.com>
2016-11-07 19:12:27 +0200

    Remove homepage

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$',
+    RedirectView.as_view(
+	pattern_name='sanitary_ware',
+	permanent=False),
+    name='home'),
     url(r'^sanitary_ware/$', views.sanitary_ware, name='sanitary_ware'),
     url(r'^acrylic_bathtubs/$', views.acrylic_bathtubs, name='acrylic_bathtubs'),
     url(r'^water_taps/$', views.water_taps, name='water_taps'),
